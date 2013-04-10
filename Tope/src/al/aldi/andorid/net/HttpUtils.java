28dd333e5d46946ac96c716ea5e23cc76a7ea862
Aldi Alimucaj <aldi@outlook.com>
2013-04-10 23:51:02 +0200

    Adding Pool Executors multithreading with return type.

--- Tope/src/al/aldi/andorid/net/HttpUtils.java
+++ Tope/src/al/aldi/andorid/net/HttpUtils.java
@@ -1,6 +1,11 @@
 package al.aldi.andorid.net;
 
 import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -15,7 +20,150 @@ import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.HttpContext;
 
 public class HttpUtils {
+
+
+    /**
+     * Sends a get request to the following url and returns true if Server
+     * responds with successful request. CODE 200
+     * @param url
+     * @return ture if code 200
+     */
     public static boolean sendGetRequest(final String url) {
+        Callable<Boolean> request = new Callable<Boolean>() {
+
+            @Override
+            public Boolean call() {
+                HttpParams httpParameters = new BasicHttpParams();
+                // Set the timeout in milliseconds until a connection is established.
+                // The default value is zero, that means the timeout is not used.
+                int timeoutConnection = 3000;
+                HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+                // Set the default socket timeout (SO_TIMEOUT)
+                // in milliseconds which is the timeout for waiting for data.
+                int timeoutSocket = 5000;
+                HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+
+                HttpClient client = new DefaultHttpClient(httpParameters);
+
+                HttpGet get = new HttpGet(url);
+                get.setHeader("Content-Type", "application/json");
+                get.addHeader("Accept", "application/json");
+                //get.setHeader("Content-Type", "text/html");
+                HttpContext localContext = new BasicHttpContext();
+                HttpResponse res = null;
+                try {
+                    res = client.execute(get, localContext);
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(res.getStatusLine());
+                return res.getStatusLine().getStatusCode() == 200;
+            }
+        };
+
+        ExecutorService pool = Executors.newFixedThreadPool(1);
+        Future<Boolean> future = pool.submit(request);
+
+        boolean success = false;
+
+        try {
+            success = future.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return success;// res.getStatusLine().getStatusCode() == 200;
+    }
+
+
+    /**
+     * After sending asynchronously the get request this method waits for
+     * the response which it then sends back to the caller.
+     * @param url URI to call
+     * @return the response from the other peer
+     */
+    public static HttpResponse sendRequest(final String url) {
+        Callable<HttpResponse> request = new Callable<HttpResponse>() {
+
+            @Override
+            public HttpResponse call() {
+                HttpParams httpParameters = new BasicHttpParams();
+                // Set the timeout in milliseconds until a connection is established.
+                // The default value is zero, that means the timeout is not used.
+                int timeoutConnection = 3000;
+                HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+                // Set the default socket timeout (SO_TIMEOUT)
+                // in milliseconds which is the timeout for waiting for data.
+                int timeoutSocket = 5000;
+                HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+
+                HttpClient client = new DefaultHttpClient(httpParameters);
+
+                HttpGet get = new HttpGet(url);
+                HttpContext localContext = new BasicHttpContext();
+                HttpResponse res = null;
+                try {
+                    res = client.execute(get, localContext);
+                } catch (ClientProtocolException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                System.out.println(res.getStatusLine());
+                return res;
+            }
+        };
+
+        ExecutorService pool = Executors.newFixedThreadPool(1);
+        Future<HttpResponse> future = pool.submit(request);
+
+        HttpResponse success = null;
+
+        try {
+            success = future.get();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+        return success;
+    }
+
+    public static boolean sendPostRequest(String url) {
+        HttpParams httpParameters = new BasicHttpParams();
+        // Set the timeout in milliseconds until a connection is established.
+        // The default value is zero, that means the timeout is not used.
+        int timeoutConnection = 3000;
+        HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+        // Set the default socket timeout (SO_TIMEOUT)
+        // in milliseconds which is the timeout for waiting for data.
+        int timeoutSocket = 5000;
+        HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+
+        HttpClient client = new DefaultHttpClient(httpParameters);
+
+        HttpPost get = new HttpPost(url);
+        HttpContext localContext = new BasicHttpContext();
+        HttpResponse res = null;
+        try {
+            res = client.execute(get, localContext);
+        } catch (ClientProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return res.getStatusLine().getStatusCode() == 200;
+    }
+
+    //*****************************************************************************//
+
+    @Deprecated
+    public static boolean sendGetRequest_old(final String url) {
         new Thread(new Runnable() {
 
             @Override
@@ -46,32 +194,6 @@ public class HttpUtils {
             }
         }).start();
 
-        return true; //res.getStatusLine().getStatusCode() == 200;
-    }
-
-    public static boolean sendPostRequest(String url) {
-        HttpParams httpParameters = new BasicHttpParams();
-        // Set the timeout in milliseconds until a connection is established.
-        // The default value is zero, that means the timeout is not used.
-        int timeoutConnection = 3000;
-        HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
-        // Set the default socket timeout (SO_TIMEOUT)
-        // in milliseconds which is the timeout for waiting for data.
-        int timeoutSocket = 5000;
-        HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
-
-        HttpClient client = new DefaultHttpClient(httpParameters);
-
-        HttpPost get = new HttpPost(url);
-        HttpContext localContext = new BasicHttpContext();
-        HttpResponse res = null;
-        try {
-            res = client.execute(get, localContext);
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return res.getStatusLine().getStatusCode() == 200;
+        return true; // res.getStatusLine().getStatusCode() == 200;
     }
 }
